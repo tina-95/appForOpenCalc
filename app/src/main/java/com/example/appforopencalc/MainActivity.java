85c237b8d9c16e0fee51ded90cd29dbbf1fc0c25
tina-95 <tina-95@mail.ru>
2021-04-06 11:27:22 +0300

    Сделано приложение, открывающее калькулятор

--- app/src/main/java/com/example/appforopencalc/MainActivity.java
+++ app/src/main/java/com/example/appforopencalc/MainActivity.java
@@ -10,25 +10,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static String TEXT = "PARAM";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uri = Uri.parse("example://intent");
-                Intent runEchoIntent = new Intent(Intent.ACTION_VIEW, uri);
-                runEchoIntent.putExtra(TEXT, "Hello");
-                ActivityInfo activityInfo = runEchoIntent.resolveActivityInfo(getPackageManager(), runEchoIntent.getFlags());
-                if (activityInfo != null) {
-                    startActivity(runEchoIntent);
-                }
-            }
-        });
-    }
 
 
 }
